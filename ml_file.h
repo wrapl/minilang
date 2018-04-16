38096ba82cb0f8c7bd325ea8e89bc2bbecd3e883
Raja Mukherji <rajamukherji@gmail.com>
2018-04-16 08:04:04 +0100

    Added function to create MLFileT objects from C code.

--- ml_file.h
+++ ml_file.h
@@ -1,10 +1,12 @@
 #ifndef ML_FILE_H
 #define ML_FILE_H
 
+#include <stdio.h>
 #include "minilang.h"
 
 void ml_file_init();
 
+ml_value_t *ml_file_new(FILE *File);
 ml_value_t *ml_file_open(void *Data, int Count, ml_value_t **Args);
 
 #endif
