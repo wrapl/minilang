4bd516541a2fbbc8b4d19a9e2803ce6eded8adaf
Raja Mukherji <rajamukherji@gmail.com>
2019-04-19 10:27:08 +0100

    Updates.

--- minilang.c
+++ minilang.c
@@ -41,19 +41,9 @@ ml_value_t *ml_load(ml_getter_t GlobalGet, void *Globals, const char *FileName) 
 	mlc_scanner_t *Scanner = ml_scanner(FileName, File, ml_file_read, Error);
 	if (setjmp(Error->Handler)) return Error->Message;
 	mlc_expr_t *Expr = ml_accept_block(Scanner);
-	ml_accept(Scanner, MLT_EOI);
+	ml_accept_eoi(Scanner);
 	fclose(File);
-	mlc_function_t Function[1] = {{Error, GlobalGet, Globals, NULL,}};
-	SHA256_CTX HashContext[1];
-	sha256_init(HashContext);
-	mlc_compiled_t Compiled = mlc_compile(Function, Expr, HashContext);
-	mlc_connect(Compiled.Exits, NULL);
-	ml_closure_t *Closure = new(ml_closure_t);
-	ml_closure_info_t *Info = Closure->Info = new(ml_closure_info_t);
-	Closure->Type = MLClosureT;
-	Info->Entry = Compiled.Start;
-	Info->FrameSize = Function->Size;
-	sha256_final(HashContext, Info->Hash);
-	if (MLDebugClosures) ml_closure_debug(Info);
+	ml_closure_t *Closure = ml_compile(Expr, GlobalGet, Globals, Error);
+	if (MLDebugClosures) ml_closure_debug(Closure->Info);
 	return (ml_value_t *)Closure;
 }
