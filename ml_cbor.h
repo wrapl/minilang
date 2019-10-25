bc020475c5999f208b8f0fcf72da3241c0ab0d78
Raja Mukherji <rajamukherji@gmail.com>
2019-10-25 12:11:50 +0100

    Updates.

--- ml_cbor.h
+++ ml_cbor.h
@@ -11,9 +11,9 @@ cbor_item_t *ml_to_cbor_item(ml_value_t *Value);
 
 typedef struct {const void *Data; size_t Length;} ml_cbor_t;
 
-typedef ml_value_t *(*ml_tag_fn_t)(uint64_t Tag, void *TagData);
+typedef ml_value_t *(ml_tag_t)(ml_value_t *Value, void *Data);
 
 ml_cbor_t ml_to_cbor(ml_value_t *Value);
-ml_value_t *ml_from_cbor(ml_cbor_t Cbor, ml_tag_fn_t TagFn, void *TagData);
+ml_value_t *ml_from_cbor(ml_cbor_t Cbor, ml_tag_t *(*TagFn)(void *TagFnData, void **TagData), void *TagFnData);
 
 #endif
