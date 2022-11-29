e735e4d4525b39da829dfc59e10b8354d648cf90
Raja Mukherji <rajamukherji@gmail.com>
2022-11-29 22:10:21 +0000

    Updates.

--- src/timelib_mem.h
+++ src/timelib_mem.h
@@ -1,7 +1,8 @@
-#ifdef TIMELIB_MEM_H
+#ifndef TIMELIB_MEM_H
 #define TIMELIB_MEM_H
 
 #include <gc/gc.h>
+#include <string.h>
 
 #define timelib_malloc GC_malloc
 #define timelib_realloc GC_realloc
@@ -12,6 +13,16 @@
 static inline void nop_free(void *Ptr) {}
 static inline void *GC_calloc(size_t N, size_t M) {
 	return GC_malloc(N * M);
+}
+
+static inline char *timelib_strndup(const char *S, size_t N) {
+	char *C;
+	size_t M;
+	M = strlen(S);
+	if (N > M) N = M;
+	C = GC_malloc_atomic(N + 1);
+	C[N] = 0;
+	return memcpy(C, S, N);
 }
 
 #endif
