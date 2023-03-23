c0801f343b0428f77282d332c0996530e3d785ba
Raja Mukherji <rajamukherji@gmail.com>
2023-03-23 09:52:51 +0000

    Updates.

--- src/ml_library.h
+++ src/ml_library.h
@@ -25,25 +25,21 @@ ml_module_t *ml_library_internal(const char *Name);
 typedef void (*ml_library_entry_t)(ml_state_t *Caller, ml_value_t **Slot);
 typedef void (*ml_library_entry0_t)(ml_value_t **Slot);
 
-#ifndef LIBRARY_NAME
-#define LIBRARY_NAME library
-#endif
-
 #define STRINGIFY(x) #x
 #define TOSTRING3(x, y, z) STRINGIFY(x ## y ## z)
 
 #define LIBRARY_ENTRY(NAME) TOSTRING3(ml_, NAME, _entry)
 #define LIBRARY_ENTRY0(NAME) TOSTRING3(ml_, NAME, _entry0)
 
-#define ML_LIBRARY_ENTRY \
-void CONCAT3(ml_, LIBRARY_NAME, entry)(ml_state_t *Caller, ml_value_t **Slot); \
-void ml_library_entry(ml_state_t *Caller, ml_value_t **Slot) __attribute__ ((weak, alias(LIBRARY_ENTRY(LIBRARY_NAME)))); \
-void CONCAT3(ml_, LIBRARY_NAME, entry)(ml_state_t *Caller, ml_value_t **Slot)
+#define ML_LIBRARY_ENTRY(NAME) \
+void CONCAT3(ml_, NAME, entry)(ml_state_t *Caller, ml_value_t **Slot); \
+void ml_library_entry(ml_state_t *Caller, ml_value_t **Slot) __attribute__ ((weak, alias(LIBRARY_ENTRY(NAME)))); \
+void CONCAT3(ml_, NAME, entry)(ml_state_t *Caller, ml_value_t **Slot)
 
-#define ML_LIBRARY_ENTRY0 \
-void CONCAT3(ml_, LIBRARY_NAME, entry0)(ml_value_t **Slot); \
-void ml_library_entry0(ml_value_t **Slot) __attribute__ ((weak, alias(LIBRARY_ENTRY0(LIBRARY_NAME)))); \
-void CONCAT3(ml_, LIBRARY_NAME, entry0)(ml_value_t **Slot)
+#define ML_LIBRARY_ENTRY0(NAME) \
+void CONCAT3(ml_, NAME, entry0)(ml_value_t **Slot); \
+void ml_library_entry0(ml_value_t **Slot) __attribute__ ((weak, alias(LIBRARY_ENTRY0(NAME)))); \
+void CONCAT3(ml_, NAME, entry0)(ml_value_t **Slot)
 
 #ifdef __cplusplus
 }
