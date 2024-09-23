e872d6811983f95b4564820bffdc5ce485104d13
Raja Mukherji <rajamukherji@gmail.com>
2024-09-23 09:53:31 +0100

    Updates.

--- src/miniwasm.c
+++ src/miniwasm.c
@@ -12,16 +12,16 @@ typedef struct {
 static ml_session_t *Sessions = NULL;
 static int NumSessions = 0, MaxSessions = 0;
 
-EM_JS(void, ml_output, (const char *text), {
-	console.log(text);
+EM_JS(void, _ml_output, (int Index, const char *Text), {
+	ml_output(Index, UTF8ToString(Text));
 });
 
-static int ml_session_print(void *Data, const char *Text, size_t Length) {
-	ml_output(Text);
-	return Length;
-}
+EM_JS(void, _ml_finish, (int Index), {
+	ml_finish(Index);
+});
 
 static void ml_session_run(ml_session_t *Session, ml_value_t *Value) {
+	if (Value == MLEndOfInput) return _ml_finish(Session - Sessions);
 	ml_stringbuffer_t Buffer[1] = {ML_STRINGBUFFER_INIT};
 	if (ml_is_error(Value)) {
 	error:
@@ -37,14 +37,16 @@ static void ml_session_run(ml_session_t *Session, ml_value_t *Value) {
 		if (ml_is_error(Value)) goto error;
 		ml_stringbuffer_write(Buffer, "\n", strlen("\n"));
 	}
-	ml_stringbuffer_drain(Buffer, NULL, ml_session_print);
+	_ml_output(Session - Sessions, ml_stringbuffer_get_string(Buffer));
+	ml_command_evaluate((ml_state_t *)Session, Session->Parser, Session->Compiler);
 }
 
-int ml_session() {
+int EMSCRIPTEN_KEEPALIVE ml_session() {
 	static int Initialized = 0;
 	static stringmap_t Globals[1] = {STRINGMAP_INIT};
 	if (!Initialized) {
 		ml_init("minilang", Globals);
+		GC_disable();
 		Initialized = 1;
 	}
 	if (NumSessions == MaxSessions) {
@@ -62,7 +64,8 @@ int ml_session() {
 	return Index;
 }
 
-void ml_session_evaluate(int Index, const char *Text) {
+void EMSCRIPTEN_KEEPALIVE ml_session_evaluate(int Index, const char *Text) {
+	GC_gcollect();
 	ml_session_t *Session = Sessions + Index;
 	ml_parser_reset(Session->Parser);
 	ml_parser_input(Session->Parser, Text, 1);
