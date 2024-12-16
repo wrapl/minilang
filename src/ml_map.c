95f33b74a41a063490074e67f250911d1acce625
Raja Mukherji <rajamukherji@gmail.com>
2024-12-16 11:39:53 +0000

    Updates.

--- src/ml_map.c
+++ src/ml_map.c
@@ -192,6 +192,7 @@ static void ml_map_template_call(ml_state_t *Caller, ml_map_t *Template, int Cou
 		ml_map_node_t *Prev = Map->Head = (ml_map_node_t *)Args[0];
 		for (int I = 1; I < Count; ++I) {
 			ml_map_node_t *Node = (ml_map_node_t *)Args[I];
+			Node->Map = Map;
 			Node->Prev = Prev;
 			Prev->Next = Node;
 			Prev = Node;
